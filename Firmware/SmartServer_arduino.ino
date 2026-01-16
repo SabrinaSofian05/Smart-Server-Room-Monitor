2c68aad80b3e6efe897c3832be899f35fc76d59f
Ilyza <137970202+ilyza12@users.noreply.github.com>
2026-01-17 03:51:35 +0800

    ESP32 Sensor code

---
+++ Firmware/SmartServer_arduino.ino
@@ -0,0 +1,80 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+// ================= USER CONFIGURATION =================
+const char* ssid     = "Galaxy A33 5GF06F";         // <--- CHANGE THIS
+const char* password = "vywa6030";     // <--- CHANGE THIS
+
+// Paste your Webhook.site URL here for testing:
+const char* serverName = "https://webhook.site/98b97d62-bdf1-4b40-81e6-11a05d51ff3c"; 
+
+const char* apiKey = "MySuperSecretKey"; // Matches the Cloud Function later
+#define DHTPIN 4
+#define DHTTYPE DHT11
+// ======================================================
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  
+  // 1. Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  
+  // Wait for connection
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Wait 10 seconds between posts
+  delay(10000);
+
+  // 2. Read Data
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // 3. Prepare JSON (The format agreed with Role B)
+  StaticJsonDocument<200> doc;
+  doc["device_id"] = "sensor_01";
+  doc["temperature"] = t;
+  doc["humidity"] = h;
+  doc["api_key"] = apiKey;
+
+  String jsonOutput;
+  serializeJson(doc, jsonOutput);
+
+  // 4. Send to Internet
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+    
+    int httpResponseCode = http.POST(jsonOutput);
+    
+    Serial.print("Sending Data: ");
+    Serial.println(jsonOutput);
+    Serial.print("Response Code: ");
+    Serial.println(httpResponseCode); // 200 means success
+    
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+}
